adca7e7ac2936f149cd4fe852a652ea069a5fa5a
MartinAntonio123 <mavp98@hotmail.com>
2020-11-04 15:12:37 -0600

    parser for any given gramar and table, harcoded lexer

--- Parser/lexer.py
+++ Parser/lexer.py
@@ -1,9 +1,45 @@
-#import sys
+
+
 class Tag:
     "class tag to relate tag names with numbers for faster results with static variables representing each case"
     EOF = 65535
-    NUMBER = 290
-    ID = 291
+    PROGRAM = 256
+    CONSTANT = 257
+    VAR = 258
+    BEGIN = 259
+    END = 260
+    INTEGER = 261
+    REAL = 262
+    BOOLEAN = 263
+    STRING = 264
+    ASSIGN = 265,
+    WRITELN = 266
+    READLN = 267
+    WHILE = 268
+    DO = 269
+    REPEAT = 270
+    UNTIL = 271
+    FOR	= 272
+    TO = 273
+    DOWNTO = 274
+    IF = 275
+    THEN = 276
+    ELSE = 277
+    NOT	= 278
+    EQ = 279
+    NEQ = 280
+    GE = 281
+    LE = 282
+    FALSE = 283
+    TRUE = 284
+    DIV = 285
+    MOD = 286
+    AND = 287
+    OR = 288
+    MINUS = 289
+    ID = 290
+    CHARACTERSTRING = 291
+    COMMMENTS = 292
     ERROR = 293
 
 class Token:
@@ -12,10 +48,78 @@ class Token:
         self.tag = tag
 
     def __str__(self):
-        if self.tag == Tag.EOF:
-            return "TOKEN, symbol = EOF"
+        if self.tag == Tag.PROGRAM:
+            return "PROGRAM"
+        elif self.tag == Tag.CONSTANT:
+            return "CONSTANT"
+        elif self.tag == Tag.VAR:
+            return "VAR"
+        elif self.tag == Tag.BEGIN:
+            return "BEGIN"
+        elif self.tag == Tag.END:
+            return "END"
+        elif self.tag == Tag.INTEGER:
+            return "INTEGER"
+        elif self.tag == Tag.REAL:
+            return "REAL"
+        elif self.tag == Tag.BOOLEAN:
+            return "BOOLEAN"
+        elif self.tag == Tag.STRING:
+            return "STRING"
+        elif self.tag == Tag.WRITELN:
+            return "WRITELN"
+        elif self.tag == Tag.READLN:
+            return "READLN"
+        elif self.tag == Tag.WHILE:
+            return "WHILE"
+        elif self.tag == Tag.DO:
+            return "DO"
+        elif self.tag == Tag.REPEAT:
+            return "REPEAT"
+        elif self.tag == Tag.UNTIL:
+            return "UNTIL"
+        elif self.tag == Tag.FOR:
+            return "FOR"
+        elif self.tag == Tag.TO:
+            return "TO"
+        elif self.tag == Tag.DOWNTO:
+            return "DOWNTO"
+        elif self.tag == Tag.IF:
+            return "IF"
+        elif self.tag == Tag.THEN:
+            return "THEN"
+        elif self.tag == Tag.ELSE:
+            return "ELSE"
+        elif self.tag == Tag.NOT:
+            return "NOT"
+        elif self.tag == Tag.DIV:
+            return "DIV"
+        elif self.tag == Tag.MOD:
+            return "MOD"
+        elif self.tag == Tag.AND:
+            return "AND";
+        elif self.tag == Tag.OR:
+            return "OR"
+        elif self.tag == Tag.EQ:
+            return "=="
+        elif self.tag == Tag.NEQ:
+            return "<>"
+        elif self.tag == Tag.LE:
+            return "<="
+        elif self.tag == Tag.GE:
+            return ">="
+        elif self.tag == Tag.MINUS:
+            return "-"
+        elif self.tag == Tag.ASSIGN:
+            return ":="
+        elif self.tag == Tag.ID:
+            return "ID"
+        elif self.tag == Tag.EOF:
+            return "EOF"
+        elif self.tag == Tag.COMMMENTS:
+            return "TOKEN - VALUE = COMMMENTS"
         else:
-            return "TOKEN, symbol = "+ chr(self.tag)
+            return "TOKEN - VALUE = "+ str(self.tag)
 
 class Word(Token):
     "For storing reserved words, returning its lexeme in string representation"
@@ -23,55 +127,179 @@ class Word(Token):
         self.tag = tag
         self.lexeme = lexeme
     def __str__(self):
-        return "WORD, lexeme = " + self.lexeme
+        return "WORD - LEXEME = " + self.lexeme
 
-class Number(Token):
+eq = Word("==", Tag.EQ)
+ne = Word("<>", Tag.NEQ)
+le = Word("<=", Tag.LE  )
+ge = Word(">=", Tag.GE )
+minus = Word("minus", Tag.MINUS )
+assing = Word(":=", Tag.ASSIGN )
+true = Word("true",  Tag.TRUE  )
+false = Word("false", Tag.FALSE )
+
+class Real(Token):
     "Data type Real for storing real numbers, this includes numbers with floating point"
     def __init__(self, value):
-        self.tag = Tag.NUMBER
+        self.tag = Tag.REAL
         self.value = value
     def __str__(self):
-        return "NUMBER, value = " + str(self.value)
+        return "REAL - VALUE = " + str(self.value)
+
+class Integer(Token):
+    "Data type Integer for storing integer values"
+    def __init__(self, value):
+        self.tag = Tag.INTEGER
+        self.value = value
+    def __str__(self):
+        return "INTEGER - VALUE = " + str(self.value)
+
+class CharacterString(Token):
+    "Data type CharacterString for storing string values"
+    def __init__(self, value):
+        self.tag = Tag.CHARACTERSTRING
+        self.value = value
+    def __str__(self):
+        return "STRING - VALUE = " + self.value
 
 class Lexer:
     "Proccesing class that opens input and reads char by char"
     def __init__(self, filename):
         self.input = open(filename, "r")
         self.reading = True
-        self.lastr = True
         self.peek = ''
         self.words = {}
-        self.lineNumber = 1
+        self.lineNumber = 0
+
+    def reserve(self, w):
+        self.words[w.lexeme] = w
+
+    def isReserved(self, t):
+        return (t in self.words)
 
     def readch(self):
         self.peek = self.input.read(1)
         if self.peek:
+            #print("scanned "+str(self.peek))
             return self.peek
         self.reading = False
         return self.peek
 
+    def bolReadch(self, c):
+        self.readch()
+        if self.peek == c:
+            return True
+        return False
+
+    def start(self):
+        self.reserve(Word("program", Tag.PROGRAM))
+        self.reserve(Word("constant", Tag.CONSTANT) )
+        self.reserve(Word("var", Tag.VAR) )
+        self.reserve(Word("begin", Tag.BEGIN) )
+        self.reserve(Word("end", Tag.END) )
+        self.reserve(Word("integer", Tag.INTEGER) )
+        self.reserve(Word("real", Tag.REAL) )
+        self.reserve(Word("boolean", Tag.BOOLEAN) )
+        self.reserve(Word("string", Tag.STRING) )
+        self.reserve(Word("writeln", Tag.WRITELN) )
+        self.reserve(Word("readln", Tag.READLN) )
+        self.reserve(Word("while", Tag.WHILE) )
+        self.reserve(Word("do", Tag.DO) )
+        self.reserve(Word("repeat", Tag.REPEAT) )
+        self.reserve(Word("until", Tag.UNTIL) )
+        self.reserve(Word("for", Tag.FOR) )
+        self.reserve(Word("to", Tag.TO) )
+        self.reserve(Word("downto", Tag.DOWNTO) )
+        self.reserve(Word("if", Tag.IF) )
+        self.reserve(Word("then", Tag.THEN) )
+        self.reserve(Word("else", Tag.ELSE) )
+        self.reserve(Word("not", Tag.NOT) )
+        self.reserve(Word("div", Tag.DIV) )
+        self.reserve(Word("mod", Tag.MOD) )
+        self.reserve(Word("and", Tag.AND) )
+        self.reserve(Word("or", Tag.OR) )
+
     def skipWhiteSpaces(self):
         while self.peek == " " or self.peek == "\t" or self.peek == "\n":
-            if self.peek == '\n':
-                self.lineNumber = self.lineNumber + 1
             self.readch()
 
-    def scan2(self):
-        mytoken = self.scan()
-        print(mytoken)
-        return mytoken
+    def readCharacterString(self):
+        cs = ""+self.peek
+        self.readch()
+        while self.peek != '"' and self.reading:
+            cs = cs + self.peek
+            self.readch()
+        cs = cs + self.peek
+        self.readch()
+        return CharacterString(cs)
 
+    def readComments(self):
+        prev = self.peek
+        self.readch()
+        while self.reading:
+            if prev == '*' and self.peek == ')':
+                self.readch()
+                break
+            prev = self.peek
+            self.readch()
+        return Token(Tag.COMMMENTS);
 
     def scan(self):
         self.skipWhiteSpaces()
+        if self.peek == '(':
+            self.readch()
+            if self.peek =='*':
+                return self.readComments()
+            return Token('(')
+        elif self.peek == '<':
+            self.readch()
+            if self.peek == '=':
+                lex.readch()
+                return le
+            elif self.peek == '>':
+                lex.readch()
+                return ne
+            else:
+                return Token('<')
+        elif self.peek == '>':
+            self.readch()
+            if self.peek == '=':
+                lex.readch()
+                return le
+            elif self.peek == '>':
+                lex.readch()
+                return ne
+            else:
+                return Token('<')
+        elif self.peek == '=':
+            self.readch()
+            if self.peek == '=':
+                lex.readch()
+                return eq
+            else:
+                return Token('=')
+        elif self.peek == ':':
+            self.readch()
+            if self.peek == '=':
+                lex.readch()
+                return assing
+            else:
+                return Token(':')
+        elif self.peek == '"':
+            return self.readCharacterString()
 
-        if self.peek.isdigit():
+        elif self.peek.isdigit():
+            real = False
             st = ""+self.peek
             self.readch()
             while self.reading and (self.peek.isdigit() or self.peek == '.'):
+                if self.peek == '.':
+                    real = True
                 st = st + self.peek
                 self.readch()
-            return Number(float(st))
+            if real:
+                return Real(float(st))
+            return Integer(int(st))
 
         elif self.peek.isalpha():
             st = ""+self.peek
@@ -83,25 +311,12 @@ class Lexer:
             if st in self.words:
                 return self.words[st]#Word(st, Tag.ID)
             w = Word(st, Tag.ID)
-            self.words[st] = w
+            self.reserve(w)
             return w
 
         else:
             if self.peek == '':
                 return Token(65535)
-            tok = Token(ord(self.peek))
+            tok = Token(self.peek)
             self.readch()
             return tok
-
-#if len(sys.argv) != 2:
-#    print("usage: lexer.py inputfile")
-#else:
-#    lex = Lexer(sys.argv[1])
-#    lex.readch()
-#    token = lex.scan()
-#    while lex.reading:
-#        print(token)
-#        token = lex.scan()
-#    print(token)
-#    print(lex.words)
-#    lex.input.close()
--- Parser/parser.py
+++ Parser/parser.py
@@ -5,67 +5,115 @@ class Parser:
     "Class to check gramar of the input and returns accepted or not"
     def __init__(self, filename):
         self.lexer = lexer.Lexer(filename)
+        self.pila = []
+        self.pilaEstados = [0]
+        self.inputstring = []
+        self.tabla = {}
+        self.fillAction("table2.csv")
+        self.manualInputString()
+        #self.fillInputString()
+
+    def fillAction(self, table):
+        arr = []
+        file = open(table, 'r')
+        Lines = file.readlines()
+        mystate = 1
+        keys = []
+        vals = []
+        for line in Lines:
+            line = line.strip()
+            line = str(line)
+            if mystate == 1:
+                keys = line.split(",")
+                mystate = 2
+            elif mystate == 2:
+                x = line.split(",")
+                for value in x:
+                    vals.append([value])
+                mystate = 3
+            elif mystate == 3:
+                x = line.split(",")
+                for i in range(len(x)):
+                    vals[i].append(x[i])
+        for i in range(len(keys)):
+            #print(i+1)
+            #print(len(vals[i]))
+            self.tabla[keys[i]] = vals[i]
+        file.close()
+
+    def manualInputString(self):
+        self.inputstring = ['id','+','id','+','(','$']
+
+    def fillInputString(self):
+        self.lexer.start()
         self.lexer.readch()
+        token = self.lexer.scan()
+        self.inputstring.append(token)
+        while self.lexer.reading:
+            print("" + str(token))
+            token = self.lexer.scan()
+            self.inputstring.append(token)
+        self.lexer.input.close()
 
-    def check(self, tag):
-        if self.token.tag == tag:
-            self.token = self.lexer.scan2()
-        else:
-            print("ERROR line"+str(self.lexer.lineNumber))
-            sys.exit()
+    def analize(self):
+        maxsteps = 0
+        while maxsteps < 50:
+            maxsteps = maxsteps + 1
+            state = self.pilaEstados[-1]
+            entrada = self.inputstring[0]
+            action = self.tabla[entrada][state]
+            print(self.pilaEstados)
+            print(self.inputstring)
+            print(self.pila)
+            print(action)
+            if action == '':
+                print("ERROR ")
+                sys.exit()
+            elif action == 'acc':
+                print("accepted")
+                sys.exit()
+            elif action[0] == 's':
+                print("shift")
+                action = action.replace('s', '')
+                action = int(action)
+                self.pilaEstados.append(action)
+                self.pila.append(entrada)
+                self.inputstring.pop(0)
+            elif action[0] == 'r':
+                print("shift")
+                action = action.replace('r', '')
+                action = int(action)
+                file = open('gramar2.txt', 'r')
+                lines = file.readlines()
+                file.close()
+                count = 0
+                reduce = ''
+                for line in lines:
+                    if count == action:
+                        reduce = line
+                        print(line)
+                    count = count + 1
+                reduce = reduce.strip()
+                reduce = reduce.split(' ')#no se
+                print(reduce)
+                while reduce[-1] != '->':
+                    self.pilaEstados.pop()
+                    if reduce.pop() != self.pila.pop():
+                        print("ERROR ")
+                        sys.exit()
+                reduce.pop()
+                self.pila.append(reduce.pop())
+                self.pilaEstados.append(int(self.tabla[self.pila[-1]][self.pilaEstados[-1]]))
 
-    def analyze(self):
-        self.token = self.lexer.scan2()
-        self.E()
-        print("ACCEPTED")
 
-    def E(self):
-        self.T()
-        self.EPrime()
 
-    def T(self):
-        self.F()
-        self.TPrime()
 
-    def EPrime(self):
-        if self.token.tag == ord('+'):
-            self.check(ord('+'))
-            self.T()
-            self.EPrime()
-        elif self.token.tag == ord('-'):
-            self.check(ord('-'))
-            self.T()
-            self.EPrime()
-        else:
-            pass
 
-    def F(self):
-        if self.token.tag == lexer.Tag.NUMBER:
-            self.check(lexer.Tag.NUMBER)
-        elif self.token.tag == lexer.Tag.ID:
-            self.check(lexer.Tag.ID)
-        elif self.token.tag == ord('('):
-            self.check(ord('('))
-            self.E()
-            self.check(ord(')'))
-        else:
-            print("ERROR line "+str(self.lexer.lineNumber))
-            sys.exit()
 
-    def TPrime(self):
-        if self.token.tag == (ord('*')):
-            self.check(ord('*'))
-            self.F()
-            self.TPrime()
-        elif self.token.tag == (ord('/')):
-            self.check(ord('/'))
-            self.T()
-            self.EPrime()
+
 
 if len(sys.argv) != 2:
     print("usage: parser.py inputfile")
 else:
-    pars = Parser(sys.argv[1])
-    pars.analyze()
-    #print(pars.lexer.words)
-    #print("n lineas = "+ str(pars.lexer.lineNumber))
+    parser = Parser(sys.argv[1])
+    parser.analize()
